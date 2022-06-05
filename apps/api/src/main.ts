91a9c6d5bd399a143783155ee4b34810453c18f1
Róbert Pánczél <robi.panczel@gmail.com>
2022-06-05 18:39:44 +0200

    Bootstrap swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,13 +3,18 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Setup Swagger for api documentation
+  bootStrapSwagger(app);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
@@ -17,6 +22,19 @@ async function bootstrap() {
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+}
+
+function bootStrapSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Secret-Manager API')
+    .setDescription(
+      'A fullstack Nx application for storing an retrieving secret keys'
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 
 bootstrap();
