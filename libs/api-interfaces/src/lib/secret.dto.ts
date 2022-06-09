3c9729ba4edabd9f26a9d9b266d5e758d77b3bbc
Róbert Pánczél <robi.panczel@gmail.com>
2022-06-09 19:15:01 +0200

    Install dependencies for nestjs validation pipe

--- libs/api-interfaces/src/lib/secret.dto.ts
+++ libs/api-interfaces/src/lib/secret.dto.ts
@@ -2,12 +2,3 @@ export class CreateSecretDto {
   secretText: string;
   remainingViews: number;
 }
-
-export const isCreateSecretDto = (obj: unknown): obj is CreateSecretDto => {
-  return (
-    obj &&
-    typeof obj === 'object' &&
-    'secretText' in obj &&
-    'remainingViews' in obj
-  );
-};
